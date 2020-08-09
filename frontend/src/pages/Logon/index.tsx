9c23ca6cf16590ce1e7ae674cac9a92ce6c261d6
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-09 03:25:56 -0300

    Concluído feature de Login de ONG

--- frontend/src/pages/Logon/index.tsx
+++ frontend/src/pages/Logon/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 import './styles.css'
 
@@ -7,18 +7,43 @@ import heroesImg from '../../assets/heroes.png'
 
 import { FiLogIn } from 'react-icons/fi'
 
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
 
 export default function Logon(){
+    const history = useHistory()
+
+    const [id, setId] = useState('')
+
+    async function handleLogin(e: FormEvent){
+        e.preventDefault()
+
+        try{
+            const response = await api.post('sessions', { id })
+
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+
+            history.push('/profile')
+        }
+        catch (err){
+            alert('Não foi possível realizar o login.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input placeholder="Sua ID" 
+                    value={id}
+                    onChange={e => setId(e.target.value)}
+                    />
 
                     <button className="button">Entrar</button>
 
