a991bace6978fcb4889a88f5a269e9dc2948ea29
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-09 04:58:54 -0300

    Desenvolvido a feature para carregar os dados de uma ONG provindos da API quando é realizado o login, fazendo uso também do localStorage; a feature de logout e a feature de deletar um dado incidente

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
+
+interface IncidentsProps {
+    id: number;
+    title: string;
+    description: string;
+    value: number;
+}
+
 
 export default function Profile() {
+    const history = useHistory()
+
+    const [incidents, setIncidents] = useState([])
+
+    const ongId = localStorage.getItem('ongId')
+    const ongName = localStorage.getItem('ongName')
+
+    function handleLogout(){
+        localStorage.clear()
+    
+        history.push('/')
+    }
+
+    async function handleDeleteIncident(id: number){
+        try{
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId
+                }
+            })
+
+            setIncidents(incidents.filter((incident: IncidentsProps) => incident.id !== id))
+        }
+        catch (err){
+            alert('Não foi possível excluir o caso!')
+        }
+    }
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        })
+        .then(response => {
+            setIncidents(response.data)
+        })
+
+    }, [ongId])
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be the Hero" />
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
             
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
@@ -23,20 +73,22 @@ export default function Profile() {
             <h1>Casos cadastrados</h1>
 
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>TITULO</p>
+                {incidents.map((incident: IncidentsProps) => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>DESCRIÇÃO</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.description}</p>
 
-                    <strong>VALOR:</strong>
-                    <p>VALOR</p>
+                        <strong>VALOR:</strong>
+                        <p>{incident.value.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'})}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
